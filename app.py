ee30b3416861fc33de6b01208ac7201b587ce3e3
Enrico Fernandes <enricofernandes@Enricos-MacBook-Air.local>
2025-04-15 12:23:42 -0300

    404

--- app.py
+++ app.py
@@ -82,6 +82,10 @@ def post(slug):
 def about():
     return render_template("about.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
 if __name__ == "__main__":
     init_db()
     app.run(debug=True)
